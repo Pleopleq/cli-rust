84e3d3df4ecee8bec511850c34e32c3b850774a5
Felix Anducho <felix.anducho74@gmail.com>
2023-10-16 00:32:17 -0600

    First implementation

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 #![allow(unused)]
 
 use clap::Parser;
+use std::fs::File;
+use std::io::{BufRead, BufReader, Read};
 
 #[derive(Parser)]
 struct CLI {
@@ -10,6 +12,19 @@ struct CLI {
 
 fn main() {
     let args = CLI::parse();
+    let file_content = File::open(&args.path).expect("Could not find file");
+    let mut reader = BufReader::new(file_content);
+
+    let mut content = String::new();
+    reader
+        .read_to_string(&mut content)
+        .expect("Cannot read string");
 
     println!("pattern: {}, path: {}", args.pattern, args.path.display());
+
+    for line in content.lines() {
+        if line.contains(&args.pattern) {
+            println!("Result: {}", line);
+        }
+    }
 }
