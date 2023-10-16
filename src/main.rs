bb382680101f7f4078ed3a858647538a64bd60bf
Felix Anducho <felix.anducho74@gmail.com>
2023-10-16 00:08:24 -0600

    Printing the cmd arguments

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,15 @@
+#![allow(unused)]
+
 use clap::Parser;
 
 #[derive(Parser)]
 struct CLI {
     pattern: String,
-    path: std::path::PathBuf
+    path: std::path::PathBuf,
 }
 
 fn main() {
-    let _args = CLI::parse();
+    let args = CLI::parse();
 
-    println!("Hello, world!");
+    println!("pattern: {}, path: {}", args.pattern, args.path.display());
 }
